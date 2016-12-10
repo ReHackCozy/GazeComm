3e0c84ad3186e2b34bcd00e6cfbe107d3524c8d1
Arman <armanizad@gmail.com>
2016-12-10 11:01:13 +0000

    added sendmessage service to send sms

---
+++ Gaze/API/GenericRest.cs
@@ -0,0 +1,20 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gaze.API
+{
+    class GenericRest<T> where T : new()
+    {
+
+        public String invoke(IRestClient client, IRestRequest request)
+        {
+            //TODO: async
+            var response = client.Execute<T>(request);
+            return response.Content; 
+        }
+    }
+}

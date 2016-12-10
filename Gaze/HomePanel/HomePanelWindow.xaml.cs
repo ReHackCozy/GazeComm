fe941bf5cfadbd568f9a968bb573006e658fea5d
cobotz <cobotz87@gmail.com>
2016-12-10 23:55:38 +0800

    Add SMS button

--- Gaze/HomePanel/HomePanelWindow.xaml.cs
+++ Gaze/HomePanel/HomePanelWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using EyeXFramework.Wpf;
 
 namespace Gaze.HomePanel
 {
@@ -20,6 +21,7 @@ namespace Gaze.HomePanel
     public partial class HomePanelWindow : Window
     {
         HomePanelViewModel vm;
+        public WpfEyeXHost eyeXHostRef;
 
         public HomePanelWindow()
         {
@@ -28,6 +30,11 @@ namespace Gaze.HomePanel
 
             this.DataContext = vm;
 
+            var currentApp = Application.Current as App;
+            eyeXHostRef = currentApp.eyeXHost;
+
+            if (eyeXHostRef == null)
+                Console.WriteLine("EyeX is NULL @ HomePanelWindow");
         }
 
         private void SuggestionBox_Initialized(object sender, EventArgs e)
@@ -37,13 +44,41 @@ namespace Gaze.HomePanel
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            //var sugg = new GazableButton();
-            //sugg.Height = 150;
-            //sugg.Width = 150;
-            //sugg.Content = "Suggestion";
-            //sugg.value = "Suggestion";
+            var sugg = new GazableButton();
+            sugg.Height = 150;
+            sugg.Width = 150;
+            sugg.Content = "Suggestion";
+            sugg.value = "Suggestion AA";
 
-            //vm.SuggestionsList.Add(sugg);
+            vm.SuggestionsList.Add(sugg);
+        }
+
+        private void OnEyeXActivate(object sender, RoutedEventArgs e)
+        {
+            var element = e.Source as FrameworkElement;
+            if (null == element) { return; }
+
+            var gazableButton = element.DataContext as GazableButton;
+
+            vm.Name = gazableButton.value;
+            SendMessageTxtBox.Text += gazableButton.value;
+        }
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.LeftShift)
+            {
+                var currentApp = Application.Current as App;
+                if (currentApp != null)
+                    eyeXHostRef.TriggerActivation();
+            }
+
+           
+        }
+
+        private void OnSendSMS(object sender, RoutedEventArgs e)
+        {
+            SendMessageTxtBox.Text += "SEND SMS";
         }
     }
 }

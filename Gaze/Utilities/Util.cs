3c2740788f80d3c85cec9ee3205ea8c55336cef1
Arman <armanizad@gmail.com>
2016-12-10 16:04:25 +0000

    added windows tts

--- Gaze/Utilities/Util.cs
+++ Gaze/Utilities/Util.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Speech.Synthesis;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +13,34 @@ namespace Gaze.Utilities
         {
             var hash = (new System.Security.Cryptography.SHA1Managed()).ComputeHash(Encoding.UTF8.GetBytes(input));
             return string.Join("", hash.Select(b => b.ToString("x2")).ToArray());
+        }
+
+        public static void Speak(String message, VoiceGender gender = VoiceGender.Male, int age = 30)
+        {
+            // Initialize a new instance of the SpeechSynthesizer.
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            synth.SelectVoiceByHints(gender, DetermineAge(age));
+            // Configure the audio output. 
+            synth.SetOutputToDefaultAudioDevice();
+            // Speak a string.
+            synth.SpeakAsync(message);
+        }
+
+        public static VoiceAge DetermineAge(int age)
+        {
+            if(age < 11)
+            {
+                return VoiceAge.Child;
+            }
+            if(age < 16)
+            {
+                return VoiceAge.Teen;
+            }
+            if(age < 45)
+            {
+                return VoiceAge.Adult;
+            }
+            return VoiceAge.Senior;
         }
     }
 }
